d40aca33454205091dde89a1306881136ba3fe79
Stallings <CS051376@cerner.net>
2020-01-08 20:36:26 -0600

    trying to commit studio 2 again

--- src/org/launchcode/java/studios/areaofacircle/Area.java
+++ src/org/launchcode/java/studios/areaofacircle/Area.java
@@ -1,4 +1,17 @@
 package org.launchcode.java.studios.areaofacircle;
 
+import java.util.Scanner;
+
 public class Area {
+    public static void main(String[] args) {
+        Scanner input;
+
+        input = new Scanner(System.in);
+        System.out.println("Enter a radius: ");
+        double radius = input.nextDouble();
+        input.close();
+
+        //double area = 3.14 * radius * radius;
+        System.out.println("The area of a circle of a radius " + radius + " is : " + Circle.getArea(radius));
+    }
 }
--- src/org/launchcode/java/studios/countingcharacters/Count.java
+++ src/org/launchcode/java/studios/countingcharacters/Count.java
@@ -1,4 +1,24 @@
 package org.launchcode.java.studios.countingcharacters;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 public class Count {
+    public static void main(String[] args) {
+        HashMap<Character, Integer> characters = new HashMap<>();
+        String name = "chelseafootballclub";
+
+        for (char i: name.toCharArray()) {
+            if (characters.containsKey(i)) {
+                int ival = characters.get(i);
+                characters.put(i, ival+1);
+            } else {
+                characters.put(i, 1);
+            }
+        }
+
+        for (Map.Entry<Character, Integer> letter : characters.entrySet()) {
+            System.out.println(letter);
+        }
+    }
 }
