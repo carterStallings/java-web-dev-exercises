47912a7600be3e3bcc125ad1455782eb807cae02
carterStallings <carterstallings1848@gmail.com>
2020-12-13 18:36:02 -0600

    Commting work for NumericTypes2 exercise

--- src/exercises/NumericTypes2.java
+++ src/exercises/NumericTypes2.java
@@ -1,4 +1,17 @@
 package exercises;
 
+import java.util.Scanner;
+
 public class NumericTypes2 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Hello, please enter the number of miles you have driven:");
+        Double miles = input.nextDouble();
+        System.out.println("Hello, please enter the amount of gas you have consumed (in gallons:");
+        Double gallons = input.nextDouble();
+        input.close();
+        Double mpg = miles/gallons;
+        System.out.println("Hello, your mpg is " + mpg + "mpg");
+
+    }
 }
