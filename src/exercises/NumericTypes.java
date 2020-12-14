6d742c32db5c863134e2d86462021669476b5aab
carterStallings <carterstallings1848@gmail.com>
2020-12-13 18:20:11 -0600

    Initial commit for exercises (HelloWorld, NumericTypes)

--- src/exercises/NumericTypes.java
+++ src/exercises/NumericTypes.java
@@ -1,4 +1,17 @@
 package exercises;
 
+import java.util.Scanner;
+
 public class NumericTypes {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Hello, please enter the length of the rectangle:");
+        Double length = input.nextDouble();
+        System.out.println("Hello, please enter the width of the rectangle:");
+        Double width = input.nextDouble();
+        input.close();
+        Double area = length * width;
+        System.out.println("Hello, the area of the rectangle is " + area);
+
+    }
 }
