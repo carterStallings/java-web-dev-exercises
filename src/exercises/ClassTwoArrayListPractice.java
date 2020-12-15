cfcaece0113f45672d7e5559ce03796593ebcae9
carterStallings <carterstallings1848@gmail.com>
2020-12-14 18:47:41 -0600

    Commiting Array and ArrayList exercise problems

--- src/exercises/ClassTwoArrayListPractice.java
+++ src/exercises/ClassTwoArrayListPractice.java
@@ -1,4 +1,40 @@
 package exercises;
 
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class ClassTwoArrayListPractice {
+    public static void main(String[] args) {
+        ArrayList<Integer> grades = new ArrayList<>();
+            grades.add(2);
+            grades.add(3);
+            grades.add(4);
+            grades.add(10);
+            grades.add(7);
+            grades.add(17);
+
+        double sum = 0;
+        for (Integer i : grades) {
+            if (i % 2 == 0) {
+                sum += i;
+            }
+        }
+        System.out.println(sum);
+
+        ArrayList<String> words = new ArrayList<>();
+            words.add("hello");
+            words.add("carter");
+            words.add("horse");
+            words.add("java");
+
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter word length:");
+        Double wordLength = input.nextDouble();
+
+        for (String i : words) {
+            if (i.length() == wordLength) {
+                System.out.println(i);
+            }
+        }
+    }
 }
--- src/exercises/ClassTwoArrayPractice.java
+++ src/exercises/ClassTwoArrayPractice.java
@@ -1,4 +1,12 @@
 package exercises;
 
 public class ClassTwoArrayPractice {
+    public static void main(String[] args) {
+        int[] someOtherInts = {1, 1, 2, 3, 5, 8};
+
+        for (int i = 0; i <someOtherInts.length; i++) {
+            if (someOtherInts[i] % 2 != 0)
+            System.out.print(someOtherInts[i]);
+        }
+    }
 }
