1d5593e3f51621ed3b64d5a6ead1cc1212047f67
carterStallings <carterstallings1848@gmail.com>
2020-12-15 18:31:40 -0600

    Committing first Class/Object exercises

--- src/org/launchcode/java/demos/lsn3classes1/Course.java
+++ src/org/launchcode/java/demos/lsn3classes1/Course.java
@@ -1,4 +1,11 @@
 package org.launchcode.java.demos.lsn3classes1;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+
 public class Course {
+    private String courseTitle;
+    private double creditHours;
+    private String instructorName;
+    private HashMap<String, Integer> students = new HashMap<>();
 }
